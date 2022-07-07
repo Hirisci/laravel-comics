19ba18466246310560c57c7ed1ca4045aa9c1901
Hirisci <www.alan92@icloud.com>
2022-07-07 18:20:38 +0200

    add btn

--- resources/views/partials/comics-list.blade.php
+++ resources/views/partials/comics-list.blade.php
@@ -9,8 +9,10 @@
                 <img class="comic-list__img" src="{{$comic['thumb']}}" alt="">
                 <div class="comic-list__title">{{$comic['series']}}</div>
             </div>
-            
         @endforeach
+       </div>
+       <div class="btn-list">
+           <button class="btn--comic-list">load more</button>
        </div>
     </div>
 </section>
